01519feaf7a94e4957e97f32c1f68ddbb47b9fc2
velitchko <velitchkofilipov@gmail.com>
2025-10-06 10:34:01 +0200

    fix css loading

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/github.io' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/github.io/' : '',
 };
 
 export default nextConfig;
